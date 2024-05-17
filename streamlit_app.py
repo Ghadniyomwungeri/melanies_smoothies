f5b2bc02cd1372ee7a526991e1865b34e5d594d7
Ghadniyomwungeri <niyomwungeri@integration-factory.de>
2024-05-17 10:01:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
 
@@ -18,14 +17,17 @@ st.write("The name of your Smoothie will be:", name_on_order)
 cnx=st.connection("snowflake")
 session=cnx.session()
 my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list= st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections=5)
 
 if ingredients_list: 
     ingredients_string =''
-    for x in ingredients_list:
-        ingredients_string += x +' '
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        # Call the Fruityvice API from Our SniS App!
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        #Put the JSON into a Dataframe
+        fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
     #st.write(ingredients_string)
     
@@ -39,9 +41,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, MellyMel!', icon="✅")
-
-
-# Call the Fruityvice API from Our SniS App!
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#Put the JSON into a Dataframe
-fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
